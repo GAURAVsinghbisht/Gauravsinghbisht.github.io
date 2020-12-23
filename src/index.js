de9f94750885b72f27abdf8138dc8d348646424b
Gaurav Singh Bisht <gbisht@covalience.com>
2020-12-23 12:27:47 +0530

    remove static port value

--- src/index.js
+++ src/index.js
@@ -101,7 +101,7 @@ io.on('connection',(socket)=>{
 // server(emit) -> client (recieve)  -- acknowledgement --> server
 // client(emit) -> server (recieve)  -- acknowledgement --> client
 
-server.listen('3000', ()=>{
+server.listen(port, ()=>{
     console.log(`Server is up & listening on port ${port}`)
 })
 
